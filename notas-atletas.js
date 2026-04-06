d7beab1f4f9ca5d10b342789b48af54fda052b12
bianca-monique <168944113+bianca-monique@users.noreply.github.com>
2026-04-06 01:19:07 -0300

    Add files via upload

---
+++ notas-atletas.js
@@ -0,0 +1,30 @@
+let atletas = [
+  { nome: "Cesar Abascal", notas: [10, 9.34, 8.42, 10, 7.88] },
+  { nome: "Fernando Puntel", notas: [8, 10, 10, 7, 9.33] },
+  { nome: "Daiane Jelinsky", notas: [7, 10, 9.5, 9.5, 8] },
+  { nome: "Bruno Castro", notas: [10, 10, 10, 9, 9.5] }
+];
+
+function calcularAtletas(objAtletas) {
+  for (let i = 0; i < objAtletas.length; i++) {
+    let atleta = objAtletas[i];
+
+    // Ordena as notas do maior para o menor
+    let notasOrdenadas = atleta.notas.sort((a, b) => b - a);
+
+    // Remove a maior (índice 0) e a menor (último) nota
+    let notasComputadas = notasOrdenadas.slice(1, 4);
+
+    // Calcula a média das 3 notas do meio
+    let soma = 0;
+    notasComputadas.forEach(nota => soma += nota);
+    let media = soma / notasComputadas.length;
+
+    console.log(`Atleta: ${atleta.nome}`);
+    console.log(`Notas Obtidas: ${notasOrdenadas}`);
+    console.log(`Média Válida: ${media}`);
+    console.log("");
+  }
+}
+
+calcularAtletas(atletas);
